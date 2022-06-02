bdc06be71c0384b744b6d9307ec742b4a4d9795a
Sukhdeep Singh <pannusukhu@gmail.com>
2022-06-02 12:38:33 +0530

    binary search tree find element

--- tree/binary-search-tree.js
+++ tree/binary-search-tree.js
@@ -19,15 +19,11 @@ class BinarySearchTree {
     }
 
     insert(key) {
-        console.log(this.root,'INSERT START FUNCTION');
         this.root = this.insertRecur(this.root, key);
-        console.log(this.root,'INSERT END FUNCTION');
     }
 
     insertRecur(root, value) {
-        console.log(root, value, 'Value')
         if (root === null) {
-            console.log('I am empty')
             root = new Node(value);
             return root;
         }
@@ -38,14 +34,88 @@ class BinarySearchTree {
         }
         return root;
     }
+
+
+    findSuccesor(node) {
+        if (node.left === null) {
+            return node;
+        } else {
+            return this.findSuccesor(node.left);
+        }
+    }
+
+    findElement(value) {
+        return this.findRecur(this.root,value);
+
+    }
+
+    findRecur(root, value) {
+        if (root === null) {
+            return null;
+        }
+        if (root.key === value) {
+            return root;
+        }
+        if (value < root.key) {
+            return this.findRecur(root.left,value);
+        } else {
+            return this.findRecur(root.right,value);
+        }
+    }
+
+    insertIter(value) {
+        const node = new Node(value)
+        let condition = true;
+
+        if (this.root === null) {
+            this.root = node;
+            return this.root;
+        }
+
+        let prevNode = null;
+        let topNode = this.root;
+
+        while (topNode !== null) {
+
+            if (value < topNode.key) {
+                prevNode = topNode;
+                topNode = topNode.left;
+            } else if (value > topNode.key) {
+                prevNode = topNode;
+                topNode = topNode.right;
+            }
+        }
+
+        if (prevNode.key > value)
+            prevNode.left = node;
+        else
+            prevNode.right = node;
+
+    }
 }
 
 
 const tree = new BinarySearchTree();
 
-tree.insert(25);
 tree.insert(2);
-tree.insert(29);
-tree.insert(4);
+tree.insert(1);
+tree.insert(0);
+tree.insert(33);
+tree.insert(25);
+tree.insert(11);
+tree.insert(7);
+tree.insert(12);
+tree.insert(13);
+tree.insert(28);
+tree.insert(26);
+tree.insert(30);
+tree.insert(40);
+tree.insert(34);
+tree.insert(36);
 
-console.log(tree, '---');
+
+
+
+// console.log(tree, '---');
+
+console.log(tree.findElement(25))
