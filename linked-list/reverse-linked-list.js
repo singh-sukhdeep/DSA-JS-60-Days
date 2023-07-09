3fe2970d94e1e31d869b54087eac05cc11dd052b
Sukhdeep Singh <pannusukhu@gmail.com>
2023-07-09 23:13:41 +0530

    reverse liked list

--- linked-list/reverse-linked-list.js
+++ linked-list/reverse-linked-list.js
@@ -1,7 +1,7 @@
 
 
 
-function Node(value) {
+function ListNode(value) {
 
     this.value = value;
     this.next = null;
@@ -13,14 +13,6 @@ function LinkedList() {
     this.head = null;
 
 }
-
-
-
-const node1 = new Node('Name 1');
-const node2 = new Node('Name 2');
-const node3 = new Node('Name 3');
-
-
 const linkedList = new LinkedList();
 
 
@@ -72,11 +64,94 @@ function insertAtEnd(linkedList, node) {
 }
 
 
-function searchNode(linkedList,node){
-    
+function searchNode(linkedList, node) {
+
 }
 
-insertAtStart(linkedList,new Node('Node 1'));
-insertAtEnd(linkedList,new Node('Node 2'));
-insertAtEnd(linkedList,new Node('Node 3'));
-console.log(linkedList);
+insertAtStart(linkedList, new ListNode('Node 1'));
+insertAtEnd(linkedList, new ListNode('Node 2'));
+const node3 = new ListNode('Node 3');
+node3.next = linkedList.head;
+insertAtEnd(linkedList, node3);
+
+
+
+// head --> A --> B --> C --> D
+
+// Step 1
+// headerPointer --> head
+// trailingPointer --> head
+
+// Step 2
+// headerPointer --> A
+// trailingPointer --> head
+
+
+// A <-- B <-- C <-- D <-- head
+
+function reverseLinkedList(linkedList) {
+    let current = linkedList.head, prev = null, next = null;
+
+    while (current !== null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next
+    }
+    linkedList.head = prev;
+
+    return linkedList;
+}
+
+
+function lengthOfLinkedList(linkedList) {
+    return linkedList.head === null ? 0 : traverse(linkedList.head)
+}
+
+function traverse(node) {
+    if (node.next === null) {
+        return 1;
+    } else {
+        return 1 + traverse(node.next);
+    }
+}
+
+function checkIfLinkedListIsCircular(linkedList) {
+
+    if (linkedList.head === null) {
+        return true;
+    }
+
+    let current = linkedList.head.next;
+    while (current != null && current != linkedList.head) {
+        current = current.next;
+    }
+
+    if (current === null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function isCircular(head) {
+    // An empty linked list is circular
+    if (head == null)
+        return true;
+
+    // Next of head
+    let node = head.next;
+
+    // This loop would stop in both cases (1) If
+    // Circular (2) Not circular
+    while (node != null && node != head)
+        node = node.next;
+
+    // If loop stopped because of circular
+    // condition
+    return (node == head);
+}
+
+// console.log(linkedList);
+console.log(checkIfLinkedListIsCircular(linkedList), '--');
+
